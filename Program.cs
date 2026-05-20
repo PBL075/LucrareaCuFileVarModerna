059c8e2a90b0c0d4029b39344b6ec7f14d60871e
PBL075 <pavelgorea75@gmail.com>
2026-05-20 17:30:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace var_modern_file
+{
+    class Program
+    {
+        static void Main()
+        {
+            string numeFisier = "note.txt";
+            string continut = "Salut! Acest text este salvat folosind metoda moderna C#.";
+
+            File.WriteAllText(numeFisier, continut);
+
+            if (File.Exists(numeFisier))
+            {
+                string textCitit = File.ReadAllText(numeFisier);
+                Console.WriteLine(textCitit);
+            }
+        }
+    }
+}
